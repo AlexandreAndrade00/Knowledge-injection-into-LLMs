e34c4131d0a8e26dc3beb75307d8a1201d2ac61f
Alexandre Andrade <alex.d.a_14@outlook.pt>
2024-10-29 17:28:15 +0000

    fix: use the correct input prompt format

--- code/src/plm/plm.py
+++ code/src/plm/plm.py
@@ -2,10 +2,18 @@ from transformers import TextGenerationPipeline
 
 
 class PLM:
-    _model: TextGenerationPipeline
+    __model: TextGenerationPipeline
 
     def __init__(self, model: TextGenerationPipeline):
-        self._model = model
+        self.__model = model
 
     def inference(self, model_input: str) -> str:
-        return self._model(model_input, max_length=100)
+        spans = self.__refined.process_text(model_input)
+
+        messages = [
+            {"role": "system",
+             "content": "You are a chatbot with the objective of answering user questions concisely!"},
+            {"role": "user", "content": model_input},
+        ]
+
+        return self.__model(messages, max_new_tokens=256)[0]["generated_text"][-1]["content"]
